020133f93237f6161206a73a6a11a948c33a1056
Luis Ventura <luis.ventura005@gmail.com>
2024-04-02 19:13:06 -0400

    - replaced daisyui with flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,14 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require("@tailwindcss/typography"),
-    require("daisyui")
+    require('flowbite/plugin')
   ],
-  daisyui: {
-    themes: ["emerald", "business"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "emerald", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
-  },
 }
 
