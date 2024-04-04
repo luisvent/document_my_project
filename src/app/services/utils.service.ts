c11f89e8828835ad0b9399152158dc59cd5423c1
Luis Ventura <luis.ventura005@gmail.com>
2024-04-03 20:12:36 -0400

    - added utils service for utility generic functions

---
+++ src/app/services/utils.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+
+import {highlight, languages} from "prismjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UtilsService {
+    constructor() {
+    }
+
+    formatCode(text: string) {
+        const html = highlight(text, languages['html'], 'html');
+        return html;
+    }
+
+    copyToClipboard(text: string) {
+        const selBox = document.createElement('textarea');
+        selBox.style.position = 'fixed';
+        selBox.style.left = '0';
+        selBox.style.top = '0';
+        selBox.style.opacity = '0';
+        selBox.value = text;
+        document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
+        selBox.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(selBox.value);
+        document.body.removeChild(selBox);
+    }
+}
