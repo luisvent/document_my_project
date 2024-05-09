f35a440eb969cc4996c52352e0c87eab8996760e
Luis Ventura <luis.ventura005@gmail.com>
2024-05-09 09:08:38 -0400

    - fixed issue setting input value on fields

--- src/app/components/multi-field/multi-field.component.ts
+++ src/app/components/multi-field/multi-field.component.ts
@@ -4,7 +4,7 @@ import {UtilsService} from "../../services/utils.service";
 @Component({
     selector: 'app-multi-field',
     templateUrl: './multi-field.component.html',
-    styleUrls: ['./multi-field.component.css']
+    styleUrls: ['./multi-field.component.css'],
 })
 export class MultiFieldComponent {
 
@@ -15,6 +15,7 @@ export class MultiFieldComponent {
     textarea: boolean = false;
     @Output()
     valueChange = new EventEmitter<string[][]>;
+
     entries: { id: string, name: string[] }[] = [];
 
     constructor(private utilsService: UtilsService) {
@@ -31,9 +32,7 @@ export class MultiFieldComponent {
             if (typeof value === 'string') {
                 this.entries.push(this.generateFields([value]));
             } else if (typeof value === 'object') {
-                Object.keys(value).forEach(key => {
-                    this.entries.push(this.generateFields([value[key], value[key]]));
-                })
+                this.entries.push(this.generateFields(Object.values(value)));
             }
         })
 
